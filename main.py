59b0653656a64c476cceef7c136d0d1ba5fa659d
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2023-07-16 01:00:40 +0900

    3.6 Python Casino

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-# my_name = "nico"
-# a = 12
-# b = "12"
-# print(my_name)
-# print(a)
-# print(b)
-# # 숫자가 변수의 제일 앞에 와선 안되고, 변수 내에서 띄어쓰기가 있어서는 안된다.
-# answer = False
-# # True or False. Boolean의 경우 첫 글자는 무조건 대문자.
-# def hotelroom(days):
-#     return 350 * days
-# def stay(days):
-#     return 200 * days
-# def tripfee(a, b):
-#     return a + b
-# room = hotelroom(3)
-# travel = stay(3)
-# totalfee = tripfee(room, travel)
-# if totalfee <= 5000:
-#     print(f"Your total fee is {totalfee}. It is appropriate. So it is admitted.")
+""""
+my_name = "nico"
+a = 12
+b = "12"
+print(my_name)
+print(a)
+print(b)
+# 숫자가 변수의 제일 앞에 와선 안되고, 변수 내에서 띄어쓰기가 있어서는 안된다.
+answer = False
+# True or False. Boolean의 경우 첫 글자는 무조건 대문자.
+def hotelroom(days):
+    return 350 * days
+def stay(days):
+    return 200 * days
+def tripfee(a, b):
+    return a + b
+room = hotelroom(3)
+travel = stay(3)
+totalfee = tripfee(room, travel)
+if totalfee <= 5000:
+    print(f"Your total fee is {totalfee}. It is appropriate. So it is admitted.")
 
-# Basic!
-# 함수 : function (parameter). / 파라미터가 들어가기 위해 선언하는 변수 : variable
-# 함수 호출 시 집어넣는 값 : argument // 함수가 돌려보내는 (return) 값 : value
+Basic!
+함수 : function (parameter). / 파라미터가 들어가기 위해 선언하는 변수 : variable
+함수 호출 시 집어넣는 값 : argument // 함수가 돌려보내는 (return) 값 : value
+
+큰 따옴표 세개 *2 => 그 사이에 내용은 주석처리 된다.
+"""
 
 
 # 2.4 Python Standard Library
@@ -29,12 +33,26 @@ from random import randint, random
 
 # phython standard library 의 random module에서 randint 함수(function)를 호출
 
-user_choice = int(input("Choose number."))
-pc_choice = randint(1, 100)
+print("Welcome to Python Casino")
+pc_choice = randint(1, 50)
+playing = True
 
-if user_choice == pc_choice:
-    print("You Won!")
-elif user_choice > pc_choice:
-    print("Lower! PC chose", pc_choice)
-elif user_choice < pc_choice:
-    print("Higher! PC chose", pc_choice)
+while playing:
+    user_choice = int(input("Choose number:"))
+    if user_choice == pc_choice:
+        print("You Won!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher!")
+
+
+""""
+[while문]
+i = 1
+while i <= 10:
+    print(f"i is {i}")
+    i = i + 1
+# while 뒤에 따라오는 조건문(condition)이 참인 동안, 그 뒤에 결과 행동이 반복적으로 실행된다.
+"""
